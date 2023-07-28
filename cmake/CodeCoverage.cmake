3cbae62210fffcc47e0075fe8e1de7093f2967db
Sergey Bronnikov <sergeyb@tarantool.org>
2023-07-28 13:30:48 +0300

    cmake: introduce a diff-cover
    The diff-cover [1] command line tool compares an XML coverage report
    with the output of git diff. It then reports coverage information for
    lines in the diff.
    
    1. https://github.com/Bachmann1234/diff_cover

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -43,3 +43,30 @@ add_custom_command(TARGET ${PROJECT_NAME}-coverage
 
 message(STATUS "Code coverage HTML report: ${COVERAGE_HTML_REPORT}")
 message(STATUS "Code coverage XML report: ${COVERAGE_XML_REPORT}")
+
+find_program(DIFFCOVER diff-cover)
+set(PATCH_COVERAGE_HTML_REPORT "${COVERAGE_DIR}/luajit-patch-coverage.html")
+set(PATCH_COVERAGE_MD_REPORT "${COVERAGE_DIR}/luajit-patch-coverage.md")
+
+if(NOT DIFFCOVER)
+  add_custom_target(${PROJECT_NAME}-patch-coverage)
+  message(WARNING "`diff-cover` is not found, \
+so ${PROJECT_NAME}-patch-coverage target is dummy")
+  return()
+endif()
+
+# XXX: Defined in a patch with checkpatch.
+set(MASTER_BRANCH "tarantool/master")
+add_custom_target(${PROJECT_NAME}-patch-coverage)
+add_custom_command(TARGET ${PROJECT_NAME}-patch-coverage
+  COMMENT "Building coverage report for a patch"
+  DEPENDS ${COVERAGE_XML_REPORT}
+  COMMAND
+    ${DIFFCOVER} ${COVERAGE_XML_REPORT}
+      --html-report ${PATCH_COVERAGE_HTML_REPORT}
+      --markdown-report ${PATCH_COVERAGE_MD_REPORT}
+      --show-uncovered
+      --compare-branch=${MASTER_BRANCH}
+      --include ${PROJECT_SOURCE_DIR}/src/*.{c,h}
+  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+)
